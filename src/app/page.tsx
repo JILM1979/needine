e3b6e5f2d4e98e6376a2a96c3371af6eecfa8c8e
JILM1979 <entradajuan@gmail.com>
2025-10-16 22:02:35 +0200

    contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@
 import React from "react";
 
 export default function Page() {
-  const scrollTo = (id: string) => {
-    document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
-  };
+  
 
   return (
     <main>
